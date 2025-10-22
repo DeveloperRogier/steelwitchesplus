131dc9fe9b532e2b1026b9e2bcf78e88f175a70a
Jonathan <1jonathanjaspers1@gmail.com>
2025-10-22 21:25:25 +0200

    feat: add turnip villager trade

--- src/main/java/net/sneezewipe/steelwitchesplus/SteelWitchesPlus.java
+++ src/main/java/net/sneezewipe/steelwitchesplus/SteelWitchesPlus.java
@@ -2,12 +2,18 @@ package net.sneezewipe.steelwitchesplus;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistryBuilder;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.potion.Potions;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradedItem;
+import net.minecraft.village.VillagerProfession;
 import net.sneezewipe.steelwitchesplus.block.ModBlockEntityTypes;
 import net.sneezewipe.steelwitchesplus.block.ModBlocks;
 import net.sneezewipe.steelwitchesplus.datagen.ModComposting;
@@ -37,14 +43,9 @@ public class SteelWitchesPlus implements ModInitializer {
 		ModLootTableModifiers.modifyLootTables();
         ModComposting.registerCompostables();
 
+		registerBrewingRecipes();
 		registerLootTableListener(EntityType.FROG, ModItems.FROG_TOE);
-
-		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-			builder.registerPotionRecipe(Potions.WATER, ModItems.INKCAP, ModPotions.DELIRIUM_POTION);
-		});
-		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
-			builder.registerPotionRecipe(Potions.WATER, ModItems.WITCHCAP, ModPotions.BEWITCHED_POTION);
-		});
+		registerTradeOffers();
 	}
 
 	private void registerLootTableListener(EntityType entity, Item newDrop) {
@@ -54,5 +55,23 @@ public class SteelWitchesPlus implements ModInitializer {
 				builder.pool(poolBuilder);
 			}
 		}));
+	}
+
+	private void registerBrewingRecipes() {
+		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(Potions.WATER, ModItems.INKCAP, ModPotions.DELIRIUM_POTION);
+		});
+		FabricBrewingRecipeRegistryBuilder.BUILD.register(builder -> {
+			builder.registerPotionRecipe(Potions.WATER, ModItems.WITCHCAP, ModPotions.BEWITCHED_POTION);
+		});
+	}
+
+	private void registerTradeOffers() {
+		TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 1, factories -> {
+			factories.add((entity, random) -> new TradeOffer(
+					new TradedItem(Items.EMERALD, 3),
+					new ItemStack(ModItems.TURNIP, 1), 7, 2, 0.04f
+			));
+		});
 	}
 }
